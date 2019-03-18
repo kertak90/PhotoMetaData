2178b89ccd1b8469f93b0a94846280c43f9555e5
kertak90 <35306203+kertak90@users.noreply.github.com>
2019-03-18 21:32:09 +0300

    Первая загрузка

---
+++ ImageOpen/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ImageOpen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.OpenBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.GPStextBox = new System.Windows.Forms.TextBox();
+            this.TakeGPS = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(327, 271);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OpenBtn
+            // 
+            this.OpenBtn.Location = new System.Drawing.Point(13, 512);
+            this.OpenBtn.Name = "OpenBtn";
+            this.OpenBtn.Size = new System.Drawing.Size(111, 98);
+            this.OpenBtn.TabIndex = 1;
+            this.OpenBtn.Text = "ОТКРЫТЬ ИЗОБРАЖЕНИЕ";
+            this.OpenBtn.UseVisualStyleBackColor = true;
+            this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(12, 290);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(328, 205);
+            this.textBox1.TabIndex = 2;
+            // 
+            // GPStextBox
+            // 
+            this.GPStextBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.GPStextBox.Location = new System.Drawing.Point(130, 535);
+            this.GPStextBox.Name = "GPStextBox";
+            this.GPStextBox.Size = new System.Drawing.Size(210, 20);
+            this.GPStextBox.TabIndex = 3;
+            // 
+            // TakeGPS
+            // 
+            this.TakeGPS.Location = new System.Drawing.Point(130, 561);
+            this.TakeGPS.Name = "TakeGPS";
+            this.TakeGPS.Size = new System.Drawing.Size(123, 49);
+            this.TakeGPS.TabIndex = 4;
+            this.TakeGPS.Text = "СКОПИРОВАТЬ В БУФЕР";
+            this.TakeGPS.UseVisualStyleBackColor = true;
+            this.TakeGPS.Click += new System.EventHandler(this.TakeGPS_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 516);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "GPS Координаты";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 573);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(347, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(441, 609);
+            this.panel1.TabIndex = 7;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(441, 609);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 634);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TakeGPS);
+            this.Controls.Add(this.GPStextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OpenBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Spyder";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button OpenBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox GPStextBox;
+        private System.Windows.Forms.Button TakeGPS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ ImageOpen/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetadataExtractor;
+
+
+namespace ImageOpen
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog OFD;
+        string GPS_Latitude_Ref = "";
+        string GPS_Latitude = "";
+        string GPS_Longitude_Ref = "";
+        string GPS_Longitude = "";
+        string Coordiante = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenBtn_Click(object sender, EventArgs e)
+        {
+            OFD = new OpenFileDialog();
+            OFD.Filter = "Файлы изображений (*.bmp, *.jpg, *.png)|*.bmp;*.jpg;*.png";
+            OFD.InitialDirectory = @"c:\";
+            OFD.Title = "Выберите интересующий вас файл изображения";
+            
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            if (OFD.ShowDialog() == DialogResult.OK)                             //Открывается окно диалога для выбора изображения
+            {
+                pictureBox1.Image = (Bitmap)Bitmap.FromFile(OFD.FileName);      //открываем в pictureBox нашего изображения
+            }
+
+            var directories = ImageMetadataReader.ReadMetadata(OFD.FileName);
+            List<string> str = new List<string>();
+            foreach (var directory in directories)
+            {
+                foreach (var tag in directory.Tags)
+                {
+                    //Console.WriteLine($"[{directory.Name}] {tag.Name} = {tag.Description}");
+                    str.Add($"[{directory.Name}] {tag.Name} = {tag.Description}");
+                    if(tag.Name == "GPS Latitude Ref")
+                        GPS_Latitude_Ref = tag.Description;
+                    if (tag.Name == "GPS Latitude")                    
+                        GPS_Latitude = tag.Description.Replace(" ", "");  
+                    if (tag.Name == "GPS Longitude Ref")
+                        GPS_Longitude_Ref = tag.Description;
+                    if (tag.Name == "GPS Longitude")
+                        GPS_Longitude = tag.Description.Replace(" ", "");
+                }                
+                    
+                if (directory.HasError)
+                {
+                    foreach (var error in directory.Errors)
+                        Console.WriteLine($"ERROR: {error}");
+                    
+                }
+            }
+            Coordiante = $"{GPS_Latitude}{GPS_Latitude_Ref} {GPS_Longitude}{GPS_Longitude_Ref}";
+            if (Coordiante == " ")
+                label1.Text = "В данном изображении нет GPS координат";
+            else
+                label1.Text = "GPS Координаты:";
+            Coordiante = Coordiante.Replace(",",".");
+            Console.WriteLine($"Координаты: {Coordiante}");
+            GPStextBox.Text = Coordiante;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Clear();                                                   //Перед выводом очистили TextBox
+            textBox1.Text = String.Join(Environment.NewLine, str);              //Поместили List<string> в TextBox
+            StringBuilder queryaddress = new StringBuilder();
+            //http://maps.google.com/maps?q=
+            queryaddress.Append("https://www.google.com/maps/place/" + Coordiante);              //https://www.google.com/maps/place/
+
+            webBrowser1.Navigate(queryaddress.ToString());
+        }
+        //https://www.google.com/maps/place/56%C2%B006'47.2%22N+46%C2%B044'26.0%22E/@56.1131113,46.738353,17z/data=!4m5!3m4!1s0x0:0x0!8m2!3d56.1131083!4d46.7405417
+        private void TakeGPS_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image!=null)
+            {                
+                label2.Text = "Координаты скопированы в буфер обмена";
+                System.Windows.Forms.Clipboard.SetText(Coordiante);
+            }            
+        }
+    }
+}
